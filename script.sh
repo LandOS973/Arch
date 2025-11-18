d119a5a3b8d99221d994a1aa50ea170e4eec2772
LandOS973 <76964599+LandOS973@users.noreply.github.com>
2025-11-18 23:51:44 +0100

    Implement conditional cloning for fzf
    Add installation check for fzf in script.sh

--- script.sh
+++ script.sh
@@ -71,7 +71,13 @@ ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
 git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
-git clone --depth=1 https://github.com/junegunn/fzf.git ~/.fzf
+
+echo "=== 🔍 Installation / configuration de fzf (GitHub) ==="
+if [ -d ~/.fzf ]; then
+  echo "Dossier ~/.fzf déjà présent, pas de clonage ✅"
+else
+  git clone --depth=1 https://github.com/junegunn/fzf.git ~/.fzf
+fi
 ~/.fzf/install --all
 
 echo "=== 🧩 Création du fichier .zshrc ==="
