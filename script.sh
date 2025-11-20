0cd3878c6867d80540aa070a5239f4331db0ede6
LandOS973 <76964599+LandOS973@users.noreply.github.com>
2025-11-20 13:03:41 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ echo "=== 🚀 Mise à jour du système ==="
 sudo pacman -Syu --noconfirm
 
 echo "=== 🧰 Installation des paquets de base ==="
-sudo pacman -S --noconfirm git curl wget base-devel zsh fzf neovim python-pip gnome-tweaks gnome-shell-extensions chromium linux-firmware
+sudo pacman -S --noconfirm git curl wget base-devel zsh fzf neovim python-pip gnome-tweaks gnome-shell-extensions chromium linux-firmware chrome-gnome-shell
 
 chromium https://extensions.gnome.org/extension/3843/just-perfection/ >/dev/null 2>&1 & disown
 
